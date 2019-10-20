77c7d6b426184dfd074af7888aa626969210d94f
Constantinos Kouloumbris <c@kouloumbris.com>
2019-10-20 10:26:46 +0100

    layout arrow fix and readme1

--- keyboards/preonic/keymaps/ploncker/keymap.c
+++ keyboards/preonic/keymaps/ploncker/keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSPC,   \
   KC_ESC,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,   \
   KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_ENTER,  \
-  FUNCTION, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_UP,   KC_DOWN,  KC_RGHT    \
+  FUNCTION, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN,   KC_UP,  KC_RGHT    \
 ),
 
 /* Lower
@@ -136,7 +136,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,  KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, KC_PGUP, KC_UP,   KC_PGDN, KC_PSCR, KC_DEL,  \
   KC_GRV,  KC_VOLD, KC_MUTE, KC_VOLU, _______, _______, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END , KC_BSLS, \
   _______, _______, _______, _______, _______, _______, KC_END , _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, KC_LBRC, KC_PIPE, KC_PIPE, KC_RBRC, KC_HOME, KC_PGUP, KC_PGDN, KC_END   \
+  _______, _______, _______, _______, KC_LBRC, KC_PIPE, KC_PIPE, KC_RBRC, KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
 )
 
 
